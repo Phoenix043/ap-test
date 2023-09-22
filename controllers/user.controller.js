02d274844346442a45b06ddcad3e2690e9377699
Phoenix043 <132455515+Phoenix043@users.noreply.github.com>
2023-09-22 18:01:37 +0530

    Add files via upload

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,38 +3,82 @@ const bcrypt=require("bcrypt")
 const UserModel = require("../models/user.model")
 
 
-exports.signUp=async (req, res) => {
-  
-    const { name, email, password, phone } = req.body;
-  
-    if (!name || !email || !phone || !password) {
-      return res.status(409).send({ message: "Please provide all fields" });
-    }
-  
-    try {
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: User authentication operations
+ */
 
-      const userWithEmailExists = await UserModel.findOne({ email });
-      if (userWithEmailExists) return res.status(409).send({message: `Email already registered`});
-      
-      const hash=bcrypt.hashSync(password, saltRounds) 
-      if (!hash) return res.status(500).send({ message: err.message })
-  
-      const user = new UserModel({
-        name,
-        email,
-        password: hashedPass,
-        phone,
-      });
-  
-      // Save the user to the database
-      await user.save();
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       description: User registration information.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: User's full name.
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: User's email address (used for registration).
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 description: User's password for authentication.
+ *               phone:
+ *                 type: string
+ *                 description: User's phone number.
+ *     responses:
+ *       201:
+ *         description: User registered successfully.
+ *       400:
+ *         description: Bad request. Please provide all required fields.
+ *       409:
+ *         description: Conflict. Email already registered.
+ *       500:
+ *         description: Internal server error. Please try again later.
+ */
 
-      res.status(201).send({ message: "User Registered Successfully" })
-        
-    } catch (error) {
-      return res.status(500).send({ message: error.message });
-    }
+exports.signUp = async (req, res) => {
+  const { name, email, password, phone } = req.body;
+
+  if (!name || !email || !phone || !password) {
+    return res.status(400).send({ message: "Please provide all required fields" });
   }
+
+  try {
+    const userWithEmailExists = await UserModel.findOne({ email });
+    if (userWithEmailExists) return res.status(409).send({ message: "Email already registered" });
+
+    const hash = bcrypt.hashSync(password, saltRounds);
+    if (!hash) return res.status(500).send({ message: "Error hashing the password" });
+
+    const user = new UserModel({
+      name,
+      email,
+      password: hash, // Fixed the variable name (hashedPass to hash)
+      phone,
+    });
+
+    // Save the user to the database
+    await user.save();
+
+    res.status(201).send({ message: "User Registered Successfully" });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
 
 
 exports.login=async (req, res) => {
